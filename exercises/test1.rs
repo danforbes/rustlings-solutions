fed6e6939e9653e35b5ef6ede6213955138cb9e3
Dan Forbes <dan.forbes@protonmail.com>
2020-01-18 06:08:41 -0800

    Test #1

--- exercises/test1.rs
+++ exercises/test1.rs
@@ -7,10 +7,13 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
+fn calculate_apple_price(orderSize: u128) -> u128 {
+    if (orderSize > 40) {
+        return orderSize;
+    }
 
-// Put your function here!
-// fn ..... {
+    return orderSize * 2;
+}
 
 // Don't modify this function!
 #[test]
